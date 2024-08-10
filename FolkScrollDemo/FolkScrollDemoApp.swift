2c9b27e22bdc060a85e944635452739affaa0cd2
Omar Rizwan <omar@omar.website>
2024-08-10 19:13:20 -0400

    Initial Commit

---
+++ FolkScrollDemo/FolkScrollDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FolkScrollDemoApp.swift
+//  FolkScrollDemo
+//
+//  Created by Omar Rizwan on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FolkScrollDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
