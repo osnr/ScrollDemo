b311f078b7e5f68451ed7629be7a0856632bd963
Omar Rizwan <omar@omar.website>
2024-08-12 03:01:48 -0400

    Publishes scroll offset to Folk

--- FolkScrollDemo/FolkScrollDemoApp.swift
+++ FolkScrollDemo/FolkScrollDemoApp.swift
@@ -10,12 +10,13 @@ import SwiftUI
 @main
 struct FolkScrollDemoApp: App {
     @StateObject private var comm = Comm()
-    init() {
-        comm.connect()
-    }
     var body: some Scene {
         WindowGroup {
-            ContentView().environmentObject(comm)
+            ContentView()
+                .onAppear() {
+                    comm.connect()
+                }
+                .environmentObject(comm)
         }
     }
 }
@@ -23,6 +24,9 @@ struct FolkScrollDemoApp: App {
 class Comm: ObservableObject {
     private var webSocketTask: URLSessionWebSocketTask!
     
+    init() {
+        print("HELLO")
+    }
     func connect() {
         let url = URL(string: "ws://localhost:4273/ws")!
         webSocketTask = URLSession.shared.webSocketTask(with: url)
@@ -30,15 +34,29 @@ class Comm: ObservableObject {
         webSocketTask?.resume()
         
         // Send establishing programs
-        self.send("Claim boop blop")
+        self.send("""
+When the scroll offset y is /offsetY/ {
+  Wish to draw text with x 100 y 100 text $offsetY
+}
+""")
     }
     func send(_ program: String) {
         let message = URLSessionWebSocketTask.Message.string(
             "Assert when websocket $chan is connected [list {this __seq} {\(program)}] with environment [list $chan [incr ::__seq]]"
         )
-        webSocketTask.send(message) {_ in }
+        webSocketTask.send(message) {err in if err != nil { print(err) }}
+    }
+    func hold(key: String, _ program: String) {
+        let message = URLSessionWebSocketTask.Message.string(
+            "Hold (non-capturing) (on folk-scroll-demo-app) {\(key)} {\(program)}"
+        )
+        webSocketTask.send(message) {err in if err != nil { print(err) }}
+            
     }
     func didScroll(scrollOffset: CGPoint) {
-        
+        if (webSocketTask != nil) {
+            self.hold(key: "scroll offset y",
+                      "Claim the scroll offset y is \(scrollOffset.y)")
+        }
     }
 }
